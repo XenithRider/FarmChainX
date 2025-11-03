6b4960a1f809c500c2b0a356dcd651e5239321b6
Sumitkumarmandal <sumitkumarmandal1020@gmail.com>
2025-11-03 13:56:59 +0530

    implement web config

---
+++ src/main/java/com/farmchain/farmchain/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.farmchain.farmchain.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
+
+}
